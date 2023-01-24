b35fd6a1d356db6501fff354cf10afd97cca1f96
Brandon <bbeam1@gmail.com>
2023-01-23 18:00:07 -0700

    add starter question logic

--- game.rb
+++ game.rb
@@ -2,17 +2,18 @@ require './player'
 require './questions'
 
 player1 = Player.new(1)
-player1.player
+#player1.player
 player2 = Player.new(2)
-player2.life_remove
-puts player2.life_count
-puts player1.life_count
-player2 = Player.new(2)
-puts player2.won
-player2.on_win
-puts player2.won
-puts player1.won
+#player2.life_remove
+#player2.life_remove
+#puts player2.life_count
+#puts player1.life_count
+#player2 = Player.new(2)
+#puts player2.won
+#player2.on_win
+#puts player2.won
+#puts player1.won
 
 
 questions1 = Questions.new()
-questions1.question
+questions1.test(1)
--- questions.rb
+++ questions.rb
@@ -1,7 +1,15 @@
+require './player'
+
 class Questions
-  def question
-    answer=$stdin.gets.chomp
-    puts "hello questions #{answer}"
+  def test(player)
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    puts "what is #{num1}+#{num2}"
+    if num1+num2==$stdin.gets.chomp.to_i
+      puts 'correct'
+    else 
+      puts 'nope' + player.to_s
+    end
   end
 end
 
